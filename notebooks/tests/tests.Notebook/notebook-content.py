22beb6e72f34bf0a063b70ed283262b26bc3fc4a
cmdruta <cm.druta@gmail.com>
2026-04-27 10:09:04 -0400

    Committing 2 items from workspace 9161a515-b087-41f4-8474-27fcb91ae1c0

---
+++ notebooks/tests/tests.Notebook/notebook-content.py
@@ -0,0 +1,23 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+spark.sql("SHOW TABLES IN bronze").show(truncate=False)
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
